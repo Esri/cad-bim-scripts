4a32c4a3f4614793e0b51ec01d2bcb5d4d317a06
Amber Birdwell <abirdwell@esri.com>
2025-07-01 08:08:29 -0700

    Update SceneLayer.py

--- Samples/Update a Scene Layer from Civil 3D Using a Layer File/SceneLayer.py
+++ Samples/Update a Scene Layer from Civil 3D Using a Layer File/SceneLayer.py
@@ -21,7 +21,7 @@ from arcgis.gis import GIS
 import time
 import os
 
-# Sign into ArcGIS Online for Arcpy & ArcGIS Python API
+# Sign in to ArcGIS Online for ArcPy and ArcGIS Python API
 print("Signing in to ArcGIS Online")
 portal = 'https://arcgis.com'
 username = 'username'
@@ -29,13 +29,13 @@ password = 'password'
 arcpy.SignInToPortal(portal, username, password)
 gis = GIS(portal, username, password)
 
-# Setup to create scene layer package from existing layer definition
+# Set up to create a scene layer package from existing layer definition
 lyrx_file = r'C:\projects\ZurichRailLines\Service_Track.lyrx'
 slpk_path = r'C:\projects\ZurichRailLines\temp'
 slpk_sr = arcpy.SpatialReference(4326)
 destinationSceneLayerID = "z3a4276hn7b224d1el987a66b7e55"
 
-# Create scene layer package with new content
+# Create a scene layer package with new content
 print("Creating scene layer package")
 baseFileName = "Service_Track" + time.strftime("%Y%m%d_%H%M%S")
 slpk_file = os.path.join(slpk_path, baseFileName + '.slpk')
@@ -48,7 +48,7 @@ search_result = gis.content.search(baseFileName, item_type="Scene Layer")
 newSceneLayerID = search_result[0].id 
 print(newSceneLayerID)
 
-# Replace existing scene layer with the updated version
+# Replace the existing scene layer with the updated version
 print("Updating destination web scene layer")
 archiveLayerName = baseFileName + '_archive'
 arcpy.server.ReplaceWebLayer(destinationSceneLayerID, archiveLayerName, newSceneLayerID, "Keep", "True")
